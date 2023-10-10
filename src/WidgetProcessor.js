bf2bf9e40cdbeeb5908be884047a1001c3629e78
yakhousam <42283612+yakhousam@users.noreply.github.com>
2023-10-10 17:31:04 +0100

    final

--- src/WidgetProcessor.js
+++ src/WidgetProcessor.js
@@ -1,64 +1,65 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import { ChevronDown, ChevronUp } from 'react-feather';
-import range from 'lodash.range';
+import React from "react";
+import { motion, LayoutGroup } from "framer-motion";
+import { ChevronDown, ChevronUp } from "react-feather";
+import range from "lodash.range";
 
-import VisuallyHidden from './VisuallyHidden';
+import VisuallyHidden from "./VisuallyHidden";
 
 function WidgetProcessor({ total }) {
+  const id = React.useId();
   const [numOfProcessedWidgets, setNumOfProcessedWidgets] = React.useState(0);
 
   const numOfUnprocessedWidgets = total - numOfProcessedWidgets;
 
   function handleProcessWidget() {
     if (numOfProcessedWidgets < total) {
-      setNumOfProcessedWidgets(numOfProcessedWidgets + 1);
+      setNumOfProcessedWidgets(numOfProcessedWidgets + 4);
     }
   }
 
   function handleRevertWidget() {
     if (numOfProcessedWidgets > 0) {
-      setNumOfProcessedWidgets(numOfProcessedWidgets - 1);
+      setNumOfProcessedWidgets(numOfProcessedWidgets - 4);
     }
   }
 
   return (
-    <div className="wrapper">
-      <div className="inbox">
-        {range(numOfUnprocessedWidgets).map((itemNum) => {
-          return (
-            <motion.div
-              layout={true}
-              key={itemNum}
-              className="widget"
-            />
-          );
-        })}
-      </div>
+    <LayoutGroup>
+      <div className="wrapper">
+        <div className="inbox">
+          {range(numOfUnprocessedWidgets).map((itemNum) => {
+            const layoutId = `${id}-${itemNum}`;
+            return (
+              <motion.div layoutId={layoutId} key={layoutId} className="widget">
+                {itemNum}
+              </motion.div>
+            );
+          })}
+        </div>
 
-      <div className="actions">
-        <button onClick={handleProcessWidget}>
-          <VisuallyHidden>Process widget</VisuallyHidden>
-          <ChevronDown />
-        </button>
-        <button onClick={handleRevertWidget}>
-          <ChevronUp />
-          <VisuallyHidden>Revert widget</VisuallyHidden>
-        </button>
-      </div>
+        <div className="actions">
+          <button onClick={handleProcessWidget}>
+            <VisuallyHidden>Process widget</VisuallyHidden>
+            <ChevronDown />
+          </button>
+          <button onClick={handleRevertWidget}>
+            <ChevronUp />
+            <VisuallyHidden>Revert widget</VisuallyHidden>
+          </button>
+        </div>
 
-      <div className="outbox">
-        {range(numOfProcessedWidgets).map((itemNum) => {
-          return (
-            <motion.div
-              layout={true}
-              key={itemNum}
-              className="widget"
-            />
-          );
-        })}
+        <div className="outbox">
+          {range(numOfUnprocessedWidgets, total).map((itemNum) => {
+            const layoutId = `${id}-${itemNum}`;
+            return (
+              <motion.div layout={layoutId} key={layoutId} className="widget">
+                {itemNum}
+              </motion.div>
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </LayoutGroup>
   );
 }
 
